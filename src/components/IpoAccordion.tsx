877887b6cdb1b8d01e13c827e10a5f0ace1b6ea4
mizuno <mzn.work.az@gmail.com>
2026-07-13 16:37:34 +0900

    component内のuserstate化

--- src/components/IpoAccordion.tsx
+++ src/components/IpoAccordion.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 // import Commponents
 import {
   Accordion,
@@ -38,135 +39,161 @@ export default function IpoAccordion({
   entryStatusList,
   lotteryResultList,
 }: Props) {
-  return (
+    /**
+     * 編集用のIPOEntry管理
+     * 画面上で「追加」「削除」「編集」を行うためのState
+     * DBに保存するまではこのStateのみ更新する
+     */
+    const [entryStateMap, setEntryStateMap] =
+    useState(() => {
+        const obj: Record<string, any[]> =
+        {};
+
+        /**
+         * Map → Object変換
+         * React Stateとして扱いやすい形へ変換
+         */
+        entryMap.forEach(
+        (value, key) => {
+            obj[key] = value;
+        }
+        );
+
+        return obj;
+  });
+    return (
     <Accordion>
     {ipoList?.map((ipo) => {
+        /**
+         * 編集対象のIPOEntry一覧
+         * 行追加、行削除、入力変更はこのStateを更新する
+         */
         const entries =
-        entryMap.get(ipo.ipo_id) ?? [];
+            entryStateMap[ipo.ipo_id] ?? [];
         return (
-        <AccordionItem
-            key={ipo.ipo_id}
-            value={ipo.ipo_id}
-        >
-        {/* Accodion外側 */}
-        <AccordionTrigger  className="bg-slate-200">
-            <div className="text-left">
-            <div className="font-semibold">
-                {ipo.company_name} 
-                {"  /評価:"} {ipo.ipo_rating}
-                {"  /状態:"} {ipo.ipo_status_name}
+            <AccordionItem
+                key={ipo.ipo_id}
+                value={ipo.ipo_id}
+            >
+                {/* Accodion外側 */}
+                <AccordionTrigger  className="bg-slate-200">
+                    <div className="text-left">
+                        <div className="font-semibold">
+                            {ipo.company_name} 
+                            {"  /評価:"} {ipo.ipo_rating}
+                            {"  /状態:"} {ipo.ipo_status_name}
 
-            </div>
-            <div>
-                {" BB:"}
-                {ipo.bb_start}
-                {" - "}
-                {ipo.bb_end}
-                {"  /上場:"}
-                {ipo.listing_date}
-                {"  /価格:"}
-                {ipo.public_price}
-            </div>
-            </div>
-        </AccordionTrigger>
-            {/* Accodion内側 */}
-            <AccordionContent>
-                {/* Accodionヘッダ */}
-                <div className="mb-4">
-                <Button>
-                    追加
-                </Button>
-                <Button>
-                    編集保存
-                </Button>
-                </div>
-             <hr />
-                <Table>
-                    {/* テーブルタイトル */}
-                    {/* <TableHeader>
-                        <TableRow>
-                        <TableHead>証券会社</TableHead>
-                        <TableHead>申込株数</TableHead>
-                        <TableHead>申込状態</TableHead>
-                        <TableHead>抽選結果</TableHead>
-                        <TableHead>メモ</TableHead>
-                        </TableRow>
-                    </TableHeader> */}
-                    {/* テーブル行 */}
-                    <TableBody>
-                        {entries.map((entry: any) => (
-                        <TableRow
-                            key={entry.ipo_entry_id}
-                        >
-                            {/* 証券会社名 */}
-                            <TableCell>
-                            {entry.security_company_name}
-                            </TableCell>
-                            
-                            {/* 申込株数 */}
-                            <TableCell>
-                            <Input
-                                defaultValue={
-                                entry.applied_shares?.toString()
-                                }
-                            />
-                            </TableCell>
-
-                            {/* 申込状態 */}
-                            <TableCell>
-                            <Select>
-                                <SelectTrigger>
-                                <SelectValue />
-                                </SelectTrigger>
-                                <SelectContent>
-                                {entryStatusList?.map((item) => (
-                                    <SelectItem
-                                    key={item.code}
-                                    value={item.code}
-                                    >
-                                    {item.code_name}
-                                    </SelectItem>
-                                ))}
-                                </SelectContent>
-                            </Select>
-                            </TableCell>
-
-                            {/* 抽選結果 */}
-                            <TableCell>
-                            <Select>
-                                <SelectTrigger>
-                                <SelectValue />
-                                </SelectTrigger>
-                                <SelectContent>
-                                {lotteryResultList?.map((item) => (
-                                    <SelectItem
-                                    key={item.code}
-                                    value={item.code}
-                                    >
-                                    {item.code_name}
-                                    </SelectItem>
-                                ))}
-                                </SelectContent>
-                            </Select>
-                            </TableCell>
-
-                            {/* メモ */}
-                            <TableCell>
-                            <Input
-                                defaultValue={entry.memo ?? ""}
-                            />
-                            </TableCell>
-                            <TableCell>
+                        </div>
+                        <div>
+                            {" BB:"}
+                            {ipo.bb_start}
+                            {" - "}
+                            {ipo.bb_end}
+                            {"  /上場:"}
+                            {ipo.listing_date}
+                            {"  /価格:"}
+                            {ipo.public_price}
+                        </div>
+                    </div>
+                </AccordionTrigger>
+                    {/* Accodion内側 */}
+                    <AccordionContent>
+                        {/* Accodionヘッダ */}
+                        <div className="mb-4">
                             <Button>
-                                削除
+                                追加
                             </Button>
-                            </TableCell>
-                        </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </AccordionContent>
-        </AccordionItem>
+                            <Button>
+                                編集保存
+                            </Button>
+                        </div>
+                    <hr />
+                    <Table>
+                        {/* テーブルタイトル */}
+                        {/* <TableHeader>
+                            <TableRow>
+                            <TableHead>証券会社</TableHead>
+                            <TableHead>申込株数</TableHead>
+                            <TableHead>申込状態</TableHead>
+                            <TableHead>抽選結果</TableHead>
+                            <TableHead>メモ</TableHead>
+                            </TableRow>
+                        </TableHeader> */}
+                        {/* テーブル行 */}
+                        <TableBody>
+                            {entries.map((entry: any) => (
+                            <TableRow
+                                key={entry.ipo_entry_id}
+                            >
+                                {/* 証券会社名 */}
+                                <TableCell>
+                                {entry.security_company_name}
+                                </TableCell>
+                                
+                                {/* 申込株数 */}
+                                <TableCell>
+                                <Input
+                                    defaultValue={
+                                    entry.applied_shares?.toString()
+                                    }
+                                />
+                                </TableCell>
+
+                                {/* 申込状態 */}
+                                <TableCell>
+                                    <Select>
+                                        <SelectTrigger>
+                                            <SelectValue />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            {entryStatusList?.map((item) => (
+                                                <SelectItem
+                                                    key={item.code}
+                                                    value={item.code}
+                                                    >
+                                                    {item.code_name}
+                                                </SelectItem>
+                                            ))}
+                                        </SelectContent>
+                                    </Select>
+                                </TableCell>
+
+                                {/* 抽選結果 */}
+                                <TableCell>
+                                    <Select>
+                                        <SelectTrigger>
+                                        <SelectValue />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                        {lotteryResultList?.map((item) => (
+                                            <SelectItem
+                                            key={item.code}
+                                            value={item.code}
+                                            >
+                                            {item.code_name}
+                                            </SelectItem>
+                                        ))}
+                                        </SelectContent>
+                                    </Select>
+                                </TableCell>
+
+                                {/* メモ */}
+                                <TableCell>
+                                    <Input
+                                        defaultValue={entry.memo ?? ""}
+                                    />
+                                </TableCell>
+                                <TableCell>
+                                    <Button>
+                                        削除
+                                    </Button>
+                                </TableCell>
+                            </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </AccordionContent>
+            </AccordionItem>
         );
     })}
     </Accordion>
